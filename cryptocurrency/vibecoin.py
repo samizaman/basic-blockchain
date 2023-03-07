a2caf14a0f8be48149e04b45f7a73f742ea22cec
Sami <sxmizaman@gmail.com>
2023-03-08 02:58:45 +0400

    POST method to connect nodes to the network

--- cryptocurrency/vibecoin.py
+++ cryptocurrency/vibecoin.py
@@ -199,7 +199,22 @@ def add_transaction():
     response = {'message': f'This transaction will be added to Block {index}'}
     return jsonify(response), 201
 
+
 # Part 3 - Decentralizing our Blockchain
+
+# Connecting new nodes to the network
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    # get the nodes from the json object
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The VibeCoin Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
 
 
 # Running the app
